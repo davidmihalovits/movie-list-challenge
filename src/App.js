0f69ce05ef537964c2ade421a49c9a3a8d0bd01f
Dávid Lajos Mihalovits <dmihalovits@gmail.com>
2021-04-15 17:30:08 +0200

    home, search, list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.sass";
+import Home from "./components/Home/Home";
 
 const App = () => {
     return (
-        <div className="App">
-            <h1>hello</h1>
+        <div>
+            <Home />
         </div>
     );
 };
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+import "./Home.sass";
+import cross from "../../assets/cross.svg";
+
+const Home = () => {
+    const [search, setSearch] = useState("");
+    const [movies, setMovies] = useState("");
+    const [genres, setGenres] = useState("");
+
+    const apiKey = "1c5abaaeaa13c66b570ad3042a0d51f4";
+
+    const getMovies = () => {
+        fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${search}&include_adult=false`,
+            {
+                method: "GET",
+            }
+        )
+            .then((res) => res.json())
+            .then((res) => setMovies(res.results));
+    };
+
+    const getGenres = () => {
+        fetch(
+            `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`,
+            {
+                method: "GET",
+            }
+        )
+            .then((res) => res.json())
+            .then((res) => setGenres(res.genres));
+    };
+
+    useEffect(() => {
+        search.length >= 3 && getGenres();
+        search.length >= 3 && getMovies();
+        search.length < 3 && getGenres("");
+        search.length < 3 && setMovies("");
+        //eslint-disable-next-line
+    }, [search]);
+
+    return (
+        <div className="home-container">
+            <div className="home-searchbar-cross">
+                <input
+                    type="text"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="home-searchbar"
+                    placeholder="Search..."
+                />
+                {search !== "" && (
+                    <img
+                        src={cross}
+                        className="home-cross"
+                        onClick={() => setSearch("")}
+                        alt="cross"
+                    />
+                )}
+                {movies &&
+                    movies.map((movie) => {
+                        return (
+                            <div
+                                key={movie.id}
+                                className="home-movie-container"
+                            >
+                                <div className="home-movie-grid">
+                                    <img
+                                        src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`}
+                                        className="movie-poster"
+                                        alt="movie poster"
+                                    />
+                                    <div className="home-movie-grid-right">
+                                        <h2 className="home-movie-grid-title">
+                                            {movie.title}
+                                        </h2>
+                                        <p className="home-movie-grid-date">
+                                            {movie.release_date}
+                                        </p>
+                                        {movie.genre_ids.map((id) => {
+                                            return genres.map(
+                                                (genre) =>
+                                                    id === genre.id && (
+                                                        <p className="home-movie-grid-genre">
+                                                            {genre.name}
+                                                        </p>
+                                                    )
+                                            );
+                                        })}
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
